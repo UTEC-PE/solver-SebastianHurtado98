0a543413d818463d9092800683d28b4f6d50a420
Sebastian Hurtado <sebastian.hurtado@utec.edu.pe>
2018-09-21 15:29:40 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,83 +1,54 @@
 #include <iostream>
-#include <map>
 #include <string>
-#include<algorithm>
-
+#include <vector>
+#include <tuple>
+#include "tree.h"
 using namespace std;
 
-void menu() {
-    system("cls");
-    cout<<"==========================================================="<<endl;
-    cout<<"\t\tPractica Operaciones"<<endl;
-    cout<<"==========================================================="<<endl;
 
-    //char* str="7/4*((a+b)*a)+3";
-    //char* str="7/4*((a+-+--b)*a)+3";
-    //char* str="2+(3)";
-    string str="7/4*((a+b)^4*a)+3";
-}
+template <typename T>
 
-void function(){
-    string str="7/4*((a+b)^4*a)+3";
-
-    int i=0, j=0;
-    map<char, int> v;
-
+tuple<int, T> pars(T str){
+    vector<pair<char, int>> v;
+    pair<char, int> foo =make_pair('e', 0);
+    int j=0;
     for (auto f : str) {
-            if (f == '(') {
-                i++;
-            }
-            if (f == ')') {
-                i--;
-            }
-        if(i==0) {
-            if ((f == '/') || (f == '*') || (f == '+') || (f == '-') || (f == '^')) {
-                pair<char, int> foo;
-                foo= make_pair(f, j);
-                v.insert(foo);
-            }
+        if ((f == '/') || (f == '*') || (f == '+') || (f == '-') || (f == '^')) {
+            foo = make_pair(f, j);
+            v.push_back(foo);
         }
         j++;
     }
 
+    int n= v.size();
     bool cnt= true;
-    char f1;
+    T f1;
     int f2;
-    auto k= v.begin();
-    while(k!=v.end()){
-        cout << (*k).first << " ";
-        k++;
-    }
-    cout << endl;
 
-    for (auto f: v){
-        if ((f.first == '+') || (f.second == '-')) {cnt= false;f1=f.first; f2= f.second; break; }
+    for (int i= 0; i<n; i++){
+        auto f=v[n-1-i];
+        if ((f.first == '+') || (f.first == '-')) {cnt= false;f1=f.first; f2= f.second; break; }
     }
     if(cnt) {
-        for (auto f: v) {
-            if ((f.first == '*') || (f.second == '/')) {cnt= false; f1=f.first; f2= f.second;break;}
+        for (int i= 0; i<n; i++) {
+            auto f=v[n-1-i];
+            if ((f.first == '*') || (f.first == '/')) {cnt= false; f1=f.first; f2= f.second;break;}
         }
     }
     if(cnt){
-        for (auto f: v) {
+        for (int i= 0; i<n; i++) {
+            auto f=v[n-1-i];
             if (f.first == '^') {cnt= false;f1=f.first; f2= f.second; break;}
         }}
 
+    return  make_tuple(f2, f1);
+};
 
-        cout << f1 << f2;
+int main() {
+
+    string str= "7+2*4^2";
+    Tree<string> tree(str);
+    cout << tree.start();
+
+    return 0;
 }
-
-
-int main(int argc, char const *argv[]) {
-    //menu();
-
-    cout << "\nPress enter to continue ...";
-
-    const char* str="7/4*((a+b)^4*a)+3";
-    function();
-
-    cin.get();
-    return EXIT_SUCCESS;
-}
-
-
---
+++ tree.h
@@ -0,0 +1,164 @@
+//
+// Created by Sebastian on 20/09/2018.
+//
+
+#ifndef SOLVER4REAL_TREE_H
+#define SOLVER4REAL_TREE_H
+#include <iostream>
+#include "node.h"
+#include <string>
+#include <vector>
+#include <regex>
+#include <tuple>
+
+using namespace std;
+
+template <typename T>
+
+class Tree {
+private:
+    Node<T> *head;
+
+    tuple<int, T> pars(T str) {
+        vector<pair<char, int>> v;
+        pair<char, int> foo;
+        int j = 0;
+        for (auto f : str) {
+            if ((f == '/') || (f == '*') || (f == '+') || (f == '-') || (f == '^')) {
+                foo = make_pair(f, j);
+                v.push_back(foo);
+            }
+            j++;
+        }
+
+        int n = v.size();
+        bool cnt = true;
+        T f1;
+        int f2;
+
+        for (int i = 0; i < n; i++) {
+            auto f = v[n - 1 - i];
+            if ((f.first == '+') || (f.first == '-')) {
+                cnt = false;
+                f1 = f.first;
+                f2 = f.second;
+                break;
+            }
+        }
+        if (cnt) {
+            for (int i = 0; i < n; i++) {
+                auto f = v[n - 1 - i];
+                if ((f.first == '*') || (f.first == '/')) {
+                    cnt = false;
+                    f1 = f.first;
+                    f2 = f.second;
+                    break;
+                }
+            }
+        }
+        if (cnt) {
+            for (int i = 0; i < n; i++) {
+                auto f = v[n - 1 - i];
+                if (f.first == '^') {
+                    f1 = f.first;
+                    f2 = f.second;
+                    break;
+                }
+            }
+        }
+
+        return {f2, f1};
+    };
+
+
+public:
+    Tree() {
+        head = nullptr;
+    };
+
+    Tree(T str) {
+        auto[position, value]= pars(str);
+        auto *n = new Node<T>();
+        n->data = value;
+        head = n;
+        string right, left;
+        int size = str.size();
+        right = str.substr(position + 1, size - position - 1);
+        left = str.substr(0, position);
+
+        create_node(left, true, head);
+        create_node(right, false, head);
+    }
+
+    void create_node(T str, bool path, Node<T> *current) {
+
+        if (str.size() == 1) {
+            auto *n = new Node<T>();
+            n->data = str;
+            if (path) {
+                current->left = n;
+            } else {
+                current->right = n;
+            }
+
+        } else {
+            auto[position, value]= pars(str);
+
+            create_node(value, path, current);
+            if (path) { current = current->left; } else { current = current->right; }
+            string right, left;
+            int size = str.size();
+            right = str.substr(position + 1, size - position - 1);
+            left = str.substr(0, position);
+
+            create_node(left, true, current);
+            create_node(right, false, current);
+        }
+
+
+    }
+    bool is_op(T f){
+        if ((f == "/") || (f == "*") || (f == "+") || (f == "-") || (f == "^")){return true;} else{return false;}
+    };
+
+
+    float operate(Node<T>* left, Node<T>* middle, Node<T>* right){
+        float L, R;
+        if(is_op(left->data)){L= operate(left->left, left, left->right);} else{L= stof(left->data);}
+        if(is_op(right->data)){R= operate(right->left, right, right->right);} else{R= stof(right->data);}
+
+        if(middle->data== "+"){
+            return L+R;
+        }
+        if(middle->data== "-"){
+            return L-R;
+        }
+        if(middle->data== "/"){
+            return L/R;
+        }
+        if(middle->data== "*"){
+            return L*R;
+        }
+        if(middle->data== "^"){
+            return pow(L,R);
+        }
+        return 0;
+
+    };
+
+
+
+    float start(){
+
+        return operate(head->left, head, head->right);
+
+    };
+
+
+
+
+};
+
+//~List();
+
+#endif //SOLVER4REAL_TREE_H
